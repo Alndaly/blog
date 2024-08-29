941ad75bcb0ad8f16d6f9db066ae079670e67133
Kinda Hall <1142704468@qq.com>
2024-08-29 20:37:15 +0800

    feat: 🎸 优化category样式

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -36,7 +36,7 @@ export const SideBar = props => {
       {/* 目录 */}
       {post?.toc && post?.toc.length > 2 && (
         <aside className='w-full rounded shadow overflow-hidden mb-6 pb-4'>
-          <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
+          <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b mb-4'>
             {locale.COMMON.TABLE_OF_CONTENTS}
           </h3>
           <Catalog toc={post?.toc} />
